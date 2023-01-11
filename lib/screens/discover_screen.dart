6211c97d0f5e9c5e29378d3d33a7176b4b6f1ec9
Your Name <chamidilshan55@gmail.com>
2023-01-11 22:28:59 +0530

    added discover page

--- lib/screens/discover_screen.dart
+++ lib/screens/discover_screen.dart
@@ -1,11 +1,47 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/bottom_navi_bar.dart';
+
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({Key? key}) : super(key: key);
 
   static const routeName = '/discover';
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.menu, color: Colors.black),
+          ),
+        ),
+        bottomNavigationBar: const BottomNavBar(index: 1),
+        body: ListView(
+          padding: EdgeInsets.all(20.0),
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Discover',
+                  style: Theme.of(context).textTheme.headline4!.copyWith(
+                    color: Colors.black,
+                      fontWeight: FontWeight.w900,
+                  ),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Text(
+                  'News from all over the world',
+                  style: Theme.of(context).textTheme.bodySmall!
+                  ),
+              ],
+            )
+          ],
+        )
+    );
   }
 }
