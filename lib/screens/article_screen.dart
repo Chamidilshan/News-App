3c08bd5b0dda2211652e4fc230ea9625e549a9c9
Your Name <chamidilshan55@gmail.com>
2023-01-14 21:45:47 +0530

    added article screen page

--- lib/screens/article_screen.dart
+++ lib/screens/article_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/models/article_model.dart';
+import 'package:news_app/widgets/image_container.dart';
+import 'package:news_app/widgets/custom_tag.dart';
 
 class ArticleScreen extends StatelessWidget {
   const ArticleScreen({Key? key}) : super(key: key);
@@ -6,6 +9,84 @@ class ArticleScreen extends StatelessWidget {
   static const routeName = '/article';
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final article = ModalRoute.of(context)!.settings.arguments as Article;
+    return ImageContainer(
+      width: double.infinity,
+      imageUrl: article.imageUrl,
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            iconTheme: IconThemeData(
+              color: Colors.white,
+            ),
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+          ),
+          body: ListView(
+            children: [
+              _NewsHeadLine(
+                article: article,
+              ),
+            ],
+          ),
+        )
+    );
+  }
+}
+
+class _NewsHeadLine extends StatelessWidget {
+  const _NewsHeadLine({
+    Key? key,
+    required this.article,
+  }) : super(key: key);
+
+  final Article article;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.15,
+          ),
+          CustomTag(
+            backgroundColor: Colors.grey.withAlpha(150),
+            children: [
+              Text(
+                article.category,
+                style:
+                Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            article.title,
+            style:
+            Theme.of(context).textTheme.bodyMedium!.copyWith(
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            article.subtitle,
+            style:
+            Theme.of(context).textTheme.bodyMedium!.copyWith(
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
